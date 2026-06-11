4c10e14cde9b1f20dd4f0e2cdffb62468384d6d6
Anuj Kushwaha <113936510+anujkus45@users.noreply.github.com>
2026-06-11 18:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // your code here
-const button = document.getElementByid("button");
-button.addEventListener("click".function()){
+const button = document.getElementById("button");
+button.addEventListener("click",function()){
 	const name = document.getElementById("name").value;
 	const year = document.getElementById("year").value;
 	let url ="https://localhost:8080/?";
 	if(name && year){
-		url += 'name=${name}&year=${year}';
+		url += `name=${name}&year=${year}`;
 	} else if(name){
-		url += 'name=${name}';
+		url += `name=${name}`;
 	} else if(year){
-		url += 'name=${year}';
+		url += `year=${year}`;
 	} else{
 		url = "https://localhost:8080/"
 	}
