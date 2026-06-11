9c50fe8441e54a14d8a411ce1216e3c170248863
Anuj Kushwaha <113936510+anujkus45@users.noreply.github.com>
2026-06-11 18:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// your code here
 const button = document.getElementById("button");
-button.addEventListener("click",function()){
-	const name = document.getElementById("name").value;
-	const year = document.getElementById("year").value;
-	let url ="https://localhost:8080/?";
-	if(name && year){
-		url += `name=${name}&year=${year}`;
-	} else if(name){
-		url += `name=${name}`;
-	} else if(year){
-		url += `year=${year}`;
-	} else{
-		url = "https://localhost:8080/"
-	}
-	document.getElementById("url").textContent = url;
+
+button.addEventListener("click", function() {
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+
+    let url = "https://localhost:8080/?";
+
+    if (name && year) {
+        url += `name=${name}&year=${year}`;
+    } else if (name) {
+        url += `name=${name}`;
+    } else if (year) {
+        url += `year=${year}`;
+    } else {
+        url = "https://localhost:8080/";
+    }
+
+    document.getElementById("url").textContent = url;
 });
