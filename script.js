9cc1293b530b1a8738d997ed280d6bd14c08f530
Anuj Kushwaha <113936510+anujkus45@users.noreply.github.com>
2026-06-11 18:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+const button = document.getElementByid("button");
+button.addEventListener("click".function()){
+	const name = document.getElementById("name").value;
+	const year = document.getElementById("year").value;
+	let url ="https://localhost:8080/?";
+	if(name && year){
+		url += 'name=${name}&year=${year}';
+	} else if(name){
+		url += 'name=${name}';
+	} else if(year){
+		url += 'name=${year}';
+	} else{
+		url = "https://localhost:8080/"
+	}
+	document.getElementById("url").textContent = url;
+});
